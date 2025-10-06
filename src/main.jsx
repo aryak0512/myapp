0df06180ccc4121d2b0ab3e8b681991c862ce92e
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-10-06 12:13:47 +0530

    Routing standardized

--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,28 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import Cart from "./components/Cart.jsx";
 import About from "./components/About.jsx";
 import Login from "./components/Login.jsx";
 import Contact from "./Contact.jsx";
 import Home from "./components/Home.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
+import App from "./App.jsx";
 
+// industry standard method of configuring routes
+const routeDefinitions = createRoutesFromElements(
+    <Route path="/" element={<App/>} errorElement={<ErrorPage/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+    </Route>
+)
 
-const appRouter = createBrowserRouter([
-
-    {
-        "path": "/",
-        "element": <App/>,
-        "errorElement": <ErrorPage/>,
-        "children": [
-            // this works as the default component in case outlet cannot find any children
-            {
-                "path": "/",
-                "index": true,
-                "element": <Home/>,
-            },
-            {
-                "path": "/",
-                "element": <Home/>,
-            },
-            {
-                "path": "/about",
-                "element": <About/>,
-            },
-            {
-                "path": "/cart",
-                "element": <Cart/>,
-            },
-            {
-                "path": "/login",
-                "element": <Login/>,
-            },
-            {
-                "path": "/contact",
-                "element": <Contact/>,
-            }
-
-        ]
-    },
-
-]);
+const appRouter = createBrowserRouter(routeDefinitions);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
